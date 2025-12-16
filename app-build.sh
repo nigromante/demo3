0c59e12d31c2555281c8a821de02cbd91aab7be8
Nigromante <nigromante@github.com>
2025-12-16 19:02:37 -0300

    update scripts

--- app-build.sh
+++ app-build.sh
@@ -2,6 +2,5 @@
 rm -rf build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build ./build
-./build/test/test
 
 
--- debug-build.sh
+++ debug-build.sh
@@ -2,7 +2,6 @@
 rm -rf debug
 cmake -S . -B debug -DCMAKE_BUILD_TYPE=Debug
 cmake --build ./debug --config Debug
-lldb-20 ./debug/test/test
 
 
 
